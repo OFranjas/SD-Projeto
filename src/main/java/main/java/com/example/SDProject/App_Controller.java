2812ee2a9c2fca549eb5ebb4eb048a2e95b2f735
OFranjas <ofranjas@gmail.com>
2023-05-15 14:32:15 +0100

    yau

--- src/main/java/main/java/com/example/SDProject/App_Controller.java
+++ src/main/java/main/java/com/example/SDProject/App_Controller.java
@@ -23,7 +23,7 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 @Controller
 public class App_Controller {
 
-    @GetMapping("/greeting")
+    @GetMapping("/menu")
     public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
             Model model) {
         model.addAttribute("name", name);
